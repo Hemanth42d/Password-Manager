6378bab1addc5839651b8635e52197f9484eb692
M V Hemanth <venkatahemanth42d@gmail.com>
2024-12-24 18:59:24 +0530

    Added seeting page and make it responsive

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express();
 
 router.get("/", (req,res) => {
-    res.render("home")
+    res.render("home");
 });
 
 router.get("/passwords", (req,res) => {
-    res.render("passwords")
+    res.render("passwords");
+})
+
+router.get("/settings", (req,res) => {
+    res.render("settings");
 })
 
 module.exports = router;
