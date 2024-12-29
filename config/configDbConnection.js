4f022dc0d1b09c32b75cf20168400f66e028a8b4
M V Hemanth <venkatahemanth42d@gmail.com>
2024-12-29 08:00:49 +0530

    final push

--- config/configDbConnection.js
+++ config/configDbConnection.js
@@ -1,13 +1,23 @@
 const mongoose = require("mongoose");
 const config = require("config");
 
-const connection = mongoose
-    .connect(config.get("MONGODB_URI"))
-    .then(()=>{
-        console.log("connected");
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
+async function connectToDatabase() {
+    try {
+        const dbUri = config.get("MONGODB_URI");
+        if (!dbUri) {
+            throw new Error("MONGODB_URI is not defined in the configuration.");
+        }
 
-module.exports = connection;
+        await mongoose.connect(dbUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log("Database connected successfully.");
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+        process.exit(1);
+    }
+}
+
+module.exports = connectToDatabase;
